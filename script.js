4f4913f771cbf76789db8ee0070a268ae4c42295
ferd78 <114067344+ferd78@users.noreply.github.com>
2022-12-17 22:22:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Please put your answer here
+
+var ButtonSignup = $(".signup-show");
+
+var modal = $('#signup-modal');
+var ButtonClose = $('#close-modal');
+
+ButtonClose.click(function() {
+    modal.fadeOut(100);
+});
+
+ButtonSignup.click(function() {
+    modal.fadeIn(100);
+});
